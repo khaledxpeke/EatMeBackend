8d5d20678ae2d5c0d6dfd7b83022323f71d703c6
khaled bouajila <khaledbouajila5481@gmail.com>
2023-03-25 23:30:55 +0100

    update feedback controller

--- controllers/feedbackController.js
+++ controllers/feedbackController.js
@@ -1,5 +1,5 @@
 const User = require("../models/user");
-const feedback = require("../models/feedbacks");
+const Feedback = require("../models/feedbacks");
 const express = require("express");
 const app = express();
 const bcrypt = require("bcryptjs");
@@ -22,16 +22,74 @@ exports.add = async (req, res, next) => {
         .status(400)
         .json({ message: "Description less than 6 characters" });
     }
-    const feedbacks = await feedback.create({
+    const feedbacks = await Feedback.create({
       description,
     });
     res.status(201).json({
-      message: "Feedback successfully created",
       feedbacks: feedbacks,
     });
   } catch (error) {
     res.status(400).json({
       message: "Feedback not successful created",
+      error: error.message,
+    });
+  }
+};
+
+exports.getAll = async (req, res, next) => {
+  try {
+    const feedbacks = await Feedback.find({});
+    res.status(200).json({
+      feedbacks,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "No Feedbacks found",
+      error: error.message,
+    });
+  }
+};
+
+exports.getFeedback = async (req, res, next) => {
+  try {
+    const feedbacks = await Feedback.findById(req.params.id);
+    res.status(200).json({
+      feedbacks,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "No Feedback found",
+      error: error.message,
+    });
+  }
+};
+
+exports.deleteFeedback = async (req, res, next) => {
+  try {
+    const feedbacks = await Feedback.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      message: "Feedback successfully deleted",
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "No Feedback found",
+      error: error.message,
+    });
+  }
+};
+
+exports.update = async (req, res, next) => {
+  const { description } = req.body;
+  try {
+    const feedbacks = await Feedback.findByIdAndUpdate(req.params.id, {
+      description,
+    });
+    res.status(200).json({
+      message: "Feedback successfully updated",
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "No Feedback found",
       error: error.message,
     });
   }
--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
-const { add } = require("../controllers/feedBackController");
+const { add,getFeedback,update,getAll,deleteFeedback } = require("../controllers/feedBackController");
 const { adminAuth,userAuth } = require("../Middleware/auth")
 
 router.route("/add").post(userAuth,add);
+router.route("/update").post(userAuth,update);
+router.route("/").get(userAuth,getAll);
+router.route("/:id").get(userAuth,getFeedback);
+router.route("/:id").delete(userAuth,deleteFeedback);
 
 
 module.exports = router;
